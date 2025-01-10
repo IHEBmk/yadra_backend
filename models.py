d8471cfa30c0de3af62be93fbdeff78d5987d488
Iheb <mekidecheiheb900@gmail.com>
2025-01-10 21:59:46 +0100

    Initial commit

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
-    supabase_id=db.Column(db.String(130), default=lambda: str(uuid.uuid4()))
+    # supabase_id=db.Column(db.String(130), default=lambda: str(uuid.uuid4()))
     id = db.Column(db.String(130), primary_key=True, default=lambda: str(uuid.uuid4()))
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
